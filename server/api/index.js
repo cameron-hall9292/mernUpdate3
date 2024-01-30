b8ba4f7fffd5821649edac599e53984c81b4c303
cameron-hall9292 <157148042+cameron-hall9292@users.noreply.github.com>
2024-01-30 11:05:29 -0600

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,44 +1,60 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const router = require('express').Router();
+//import5 todo model
 
-const dotenv = require ('dotenv').config();
-const cors = require("cors");
-
-const app = express();
-
-// use express.json() to get data into json format
-
-app.use(express.json());
-
-//Port
-
-const PORT = process.env.PORT || 5500;
-
-//use cors
-app.use(cors());
-
-//Lets import routes
-
-const TodoItemRouter = require('./routes/todoItems');
-
-//Lets connect to mongodb
-
-mongoose.connect(process.env.DB_CONNECT)
-.then(()=> console.log("Database connected"))
-.catch(err => console.log(err));
-
-app.use('/', TodoItemRouter)
+const todoItemsModel = require('../models/todoItems')
 
 
-//add port and connect to server
+//Lets create our first route -- we will ad todo item to our DB
+
+router.post('/api/item', async (req,res)=> {
+    try{
+        const newItem = new todoItemsModel({
+            item: req.body.item
+        });
+        //save this item in DB
+        const saveItem =   await newItem.save()
+        res.status(200).json(saveItem)
+    } catch(err){
+        res.json(err);
+    }
+});
+
+//Lets create a second route -- get data from DB
+
+router.get('/api/items', async (req,res) => {
+    try {
+        const allTodoItems = await todoItemsModel.find({});
+        res.status(200).json(allTodoItems);
+    }catch(err){
+        res.json(err);
+    }
+});
+
+//Let's update item
+router.put('/api/item/:id', async (req, res)=> {
+    try{
+    const updateItem = await todoItemsModel.findByIdAndUpdate(req.params.id, {$set: req.body});
+    res.status(200).json('Item updated');
+    }catch(err){
+        res.json(err);
+    }
 
 
-app.listen(PORT, (err) => {
-    if (err) console.log(err); else console.log('Server is running on port:', PORT);}
-)
+});
+
+//Let's update item
+router.delete('/api/item/:id', async (req, res)=> {
+    try{
+    const updateItem = await todoItemsModel.findByIdAndDelete(req.params.id, {$set: req.body});
+    res.status(200).json('Item deleted');
+    }catch(err){
+        res.json(err);
+    }
 
 
-// app.listen(PORT, '0.0.0.0'); // or server.listen(3001, '0.0.0.0'); for all interfaces
-// app.on('listening', function() {
-//     console.log('Express server started on port %s at %s', server.address().port, server.address().address);
-// });
+})
+
+
+//export router
+
+module.exports = router;
