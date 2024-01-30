8430d73b6ab8480d041169ae4d10f814e4d8d962
cameron-hall9292 <157148042+cameron-hall9292@users.noreply.github.com>
2024-01-30 11:59:34 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ mongoose.connect(process.env.DB_CONNECT)
 .then(()=> console.log("Database connected"))
 .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+    res.json("Hello");
+});
+
 app.use('/', TodoItemRouter)
 
 // app.get("/", (res, res) => {
