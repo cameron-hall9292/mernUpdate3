15b346abe6dd13c720a2878183356fc45c1615b8
cameron-hall9292 <157148042+cameron-hall9292@users.noreply.github.com>
2024-02-03 16:41:27 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const dotenv = require ('dotenv').config();
 const cors = require("cors");
 
+
 const app = express();
+
+const todoItemsModel = require('./models/todoItems')
 
 // use express.json() to get data into json format
 
@@ -30,7 +33,19 @@ mongoose.connect(process.env.DB_CONNECT)
 .then(()=> console.log("Database connected"))
 .catch(err => console.log(err));
 
-app.use('/', TodoItemRouter)
+
+
+
+router.get('/api/items', async (req,res) => {
+    try {
+        const allTodoItems = await todoItemsModel.find({});
+        res.status(200).json(allTodoItems);
+    }catch(err){
+        res.json(err);
+    }
+});
+
+//app.use('/', TodoItemRouter)
 
 
 //add port and connect to server
